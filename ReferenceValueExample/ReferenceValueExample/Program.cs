d45ea60e3c2a69988a27cc13cf0c02be60a2b2ca
majji82 <2100032153cseh@gmail.com>
2024-12-26 12:33:53 +0530

    Ex of Reference

---
+++ ReferenceValueExample/ReferenceValueExample/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ReferenceValueExample
+{
+    public class Person
+    {
+        public int Age;
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            Increment(num);
+            Console.WriteLine(num);
+
+            Person p = new Person() { Age = 10 };
+            IncrementAge(p); 
+            Console.WriteLine(p.Age);
+
+        }
+
+        public static void Increment(int val)
+        {
+            val += 10;
+        }
+
+        public static void IncrementAge(Person p)
+        {
+            p.Age += 10;
+        }
+    }
+}
