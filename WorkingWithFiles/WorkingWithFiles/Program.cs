ca2066425efc06f6dd4a8057e67bc52be5b29e49
majji82 <2100032153cseh@gmail.com>
2024-12-28 07:22:44 +0530

    done with basics of c#

---
+++ WorkingWithFiles/WorkingWithFiles/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace WorkingWithFiles
+{
+    class Program
+    {
+
+        static void FileOps()
+        {
+
+            var path = @"c:\somefile.jpg";
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+
+            if (File.Exists(path))
+            {
+                //
+            }
+
+            var content = File.ReadAllText(path);
+
+            var fi = new FileInfo(path);
+            fi.CopyTo("..");
+            fi.Delete();
+
+            if (fi.Exists)
+            {
+                //
+            }
+        }
+
+        static void DirectoryOps()
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+            var files = Directory.GetFiles(@"c:\Users\Lenovo\Desktop\CSharpBasic", "*.sln", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                //Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories(@"c:\Users\Lenovo\Desktop\CSharpBasic", "*", SearchOption.AllDirectories);
+            foreach(var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+        }
+
+        static void pathOps()
+        {
+            var path = @"c:\Users\Lenovo\Desktop\CSharpBasic\WorkinggWithFiles\WorkingWithFiles.sln";
+
+            Console.WriteLine(Path.GetExtension(path));
+            Console.WriteLine(Path.GetFileName(path));
+            Console.WriteLine(Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine(Path.GetDirectoryName(path));
+        }
+        static void Main(string[] args)
+        {
+            //FileOps();
+            //DirectoryOps();
+            pathOps();
+        }
+    }
+}
